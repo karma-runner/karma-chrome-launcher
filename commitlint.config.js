58c8874cc53252d3605ef02cadeb508ccee74765
Jonathan Ginsburg <jon@than.ml>
2022-02-15 00:36:52 -0600

    ci: add the "Release" GitHub Actions workflow
    Also:
    
    * Remove the "pretest" npm script as it is explicitly called in the "Test"
    GitHub Actions workflow.
    * Allow for the "chore" commit type as it is used by the grunt "contributors"
    task.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-module.exports = { extends: ['@commitlint/config-angular'] }
+const angularConfig = require('@commitlint/config-angular')
+module.exports = {
+  rules: {
+    ...angularConfig.rules,
+    'type-enum': [
+      2,
+      'always',
+      // Also allow the "chore" commit type.
+      ['chore'].concat(angularConfig.rules['type-enum'][2])
+    ]
+  }
+}
